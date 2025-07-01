8e043d6f06d207025bdf527fca3f7df098c2d639
Marco Antonio Filho <m.barceloslima@gmail.com>
2025-07-01 12:29:04 -0700

    Update user request validation and improve error handling in user creation

--- src/controller/create_user.go
+++ src/controller/create_user.go
@@ -1,21 +1,32 @@
 package controller
 
 import (
-	"fmt"
+	"log"
 
 	"github.com/gin-gonic/gin"
-	resterr "github.com/marcofilho/go-crud-api/src/configuration/rest_err"
+	"github.com/google/uuid"
+	"github.com/marcofilho/go-crud-api/src/configuration/rest_err"
 	"github.com/marcofilho/go-crud-api/src/controller/model/request"
+	"github.com/marcofilho/go-crud-api/src/controller/model/response"
 )
 
 func CreateUser(c *gin.Context) {
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := resterr.NewBadRequestError(fmt.Sprintf("Invalid user request, error: %s", err))
+		log.Printf("Error trying to marshal user request, error: %s", err.Error())
+		restErr := rest_err.NewBadRequestError("Some fields are invalid")
+
 		c.JSON(restErr.Code, restErr)
 		return
 	}
 
-	fmt.Println(userRequest)
+	userResponse := response.UserResponse{
+		ID:    uuid.NewString(),
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+
+	c.JSON(200, &userResponse)
 }
