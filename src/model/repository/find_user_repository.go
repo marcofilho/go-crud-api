af7910547d2705b8d3b215399de545488b3b1d30
Marco Antonio Filho <m.barceloslima@gmail.com>
2025-07-09 16:55:43 -0700

    Fix FindUserByID filter: Change filter key from "id" to "_id" and ensure ObjectID conversion for MongoDB query.

--- src/model/repository/find_user_repository.go
+++ src/model/repository/find_user_repository.go
@@ -11,6 +11,7 @@ import (
 	"github.com/marcofilho/go-crud-api/src/model/repository/entity"
 	"github.com/marcofilho/go-crud-api/src/model/repository/entity/converter"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.uber.org/zap"
 )
@@ -25,7 +26,9 @@ func (ur *userRepository) FindUserByID(id string) (model.UserDomainInterface, *r
 
 	userEntity := entity.UserEntity{}
 
-	filter := bson.D{{Key: "id", Value: id}}
+	objectID, _ := primitive.ObjectIDFromHex(id)
+
+	filter := bson.D{{Key: "_id", Value: objectID}}
 	err := collection.FindOne(ctx, filter).Decode(&userEntity)
 
 	if err != nil {
