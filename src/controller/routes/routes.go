07b5172627a2612f1f5d04c77fde22f6c4cdfbd4
Marco Antonio Filho <m.barceloslima@gmail.com>
2025-07-10 11:38:21 -0700

    Add route for retrieving all users: Implement GET endpoint for FindAllUsers in routes.

--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -8,6 +8,7 @@ import (
 func InitRoutes(r *gin.RouterGroup, userController controller.UserControllerInterface) {
 	r.GET("/getUserByID/:userID", userController.FindUserByID)
 	r.GET("/getUserByEmail/:email", userController.FindUserByEmail)
+	r.GET("getAllUsers/", userController.FindAllUsers)
 	r.POST("/createUser", userController.CreateUser)
 	r.PUT("/updateUser/:userId", userController.UpdateUser)
 	r.DELETE("/deleteUser/:userId", userController.DeleteUser)
